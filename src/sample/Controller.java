71b3b044ba51e2c2f46f0043025ff470a5860961
reuven9632 <reuven9632@gmail.com>
2020-11-30 23:59:10 +0200

    make shablone wondow

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,37 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button authButton;
+
+    @FXML
+    private TextField login_field;
+
+    @FXML
+    private Button registrationButoon;
+
+    @FXML
+    private PasswordField password_field;
+
+    @FXML
+    void initialize() {
+        authButton.setOnAction(event -> {
+
+        });
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hi Alona aa !!!");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("My first app");
+        primaryStage.setScene(new Scene(root, 800, 400));
         primaryStage.show();
     }
 
