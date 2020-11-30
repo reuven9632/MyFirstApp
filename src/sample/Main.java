0d98b7169c7c707ac2cd4454a7a749fe4bd95508
reuven9632 <reuven9632@gmail.com>
2020-12-01 01:26:48 +0200

    make shablone wondow 1.3 fixed

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("My first app11");
+        primaryStage.setTitle("My first app");
         primaryStage.setScene(new Scene(root, 954, 645));
         primaryStage.show();
     }
