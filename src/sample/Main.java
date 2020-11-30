b1e07b722161c176a1581c5ae98f5609ab06fc59
reuven9632 <reuven9632@gmail.com>
2020-11-30 22:28:12 +0200

    add javafx libreris

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,9 +9,9 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hi !!!");
+        primaryStage.setTitle("Hi Alona !!!");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
